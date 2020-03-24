17321a58f027ffd7392bb011e79b46f29436fd2e
majudhu <majudhu.sup@gmail.com>
2020-03-24 22:42:24 +0500

    kjsndkjnaksjdnvadf

--- app/src/main/java/com/example/gt168_kotlin/GT168Controller.kt
+++ app/src/main/java/com/example/gt168_kotlin/GT168Controller.kt
@@ -1,20 +1,21 @@
 package com.example.gt168_kotlin
 
-
 import android.hardware.usb.*
 import kotlinx.coroutines.*
 
 class GT168Controller(private val usbManager: UsbManager, private val device: UsbDevice) {
 
     companion object {
-        private const val VID = 0x2009
-        private const val PID = 0x7638
+        private const val VID = 0x2009  // USB Vendor ID
+        private const val PID = 0x7638  // USB Product ID
 
-        private const val USB_CBW_SIG = 0x43425355
-        private const val USB_CBW_TUN = 0x89182B28
-        private const val USB_CBW_IN = 0x00.toByte()
-        private const val USB_CBW_OUT = 0x80.toByte()
-        private const val USB_CBW_LUN = 0x00.toByte()
+        private const val CBW_SIG = 0x43425355          // Command Block Wrapper Signature
+        private const val CBW_TUN = 0x89182B28          // Transaction Unique Identifier
+        private const val CBW_IN = 0x00.toByte()  // Direction: to Device
+        private const val CBW_OUT = 0x80.toByte() // Direction: to Host
+        private const val CBW_LUN = 0x00.toByte() // Logical Unit Number
+        private const val CBW_LEN = 0x0A.toByte() // Command Block Length
+        val CBW_DATA get() = byteArrayOf(0xEF.toByte(), 0x11, 0x00, 0x00, 0x18)
 
         fun getDevice(usbManager: UsbManager): UsbDevice? = usbManager
             .deviceList.values.firstOrNull { it.vendorId == VID && it.productId == PID }
@@ -32,7 +33,6 @@ class GT168Controller(private val usbManager: UsbManager, private val device: Us
             ((this ushr 16) and 0xFFFF).toByte(),
             ((this ushr 24) and 0xFFFF).toByte()
         )
-
     }
 
     private val usbInterface = device.getInterface(0)
@@ -51,17 +51,18 @@ class GT168Controller(private val usbManager: UsbManager, private val device: Us
 
     private val maxOutSize = outEndpoint.maxPacketSize
 
+    suspend fun sendCommand
 
     suspend fun testConnection(): Boolean = withContext(Dispatchers.IO) {
         val res = ByteArray(64)
         val csw = ByteArray(64)
         val usbInterface = device.getInterface(0)
         val cbw = byteArrayOf(
-            *USB_CBW_SIG.toByteArray(),
-            *USB_CBW_TUN.toByteArray(),
+            *CBW_SIG.toByteArray(),
+            *CBW_TUN.toByteArray(),
             *0.toByteArray(),
-            USB_CBW_IN,
-            USB_CBW_LUN,
+            CBW_IN,
+            CBW_LUN,
             0x0A,
             0x11,
             0x00,
@@ -77,7 +78,7 @@ class GT168Controller(private val usbManager: UsbManager, private val device: Us
         val r1 = usbDeviceConnection.bulkTransfer(outEndpoint, cbw, 31, 3000)
         val r2 = usbDeviceConnection.bulkTransfer(outEndpoint, cmd, 24, 3000)
         val r3 = usbDeviceConnection.bulkTransfer(inEndPoint, csw, 13, 3000)
-        cbw[0x0c] = USB_CBW_OUT
+        cbw[0x0c] = CBW_OUT
         cbw[0x10] = 0x12.toByte()
         val r4 = usbDeviceConnection.bulkTransfer(outEndpoint, cbw, 31, 3000)
         val r5 = usbDeviceConnection.bulkTransfer(inEndPoint, res, 24, 3000)
